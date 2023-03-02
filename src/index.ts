6b839fb42af8ea6b305508f26986de0fcd41817a
Janaina-Costa <janainacosta.dev@gmail.com>
2023-03-02 19:50:29 -0300

    configura o servidor e add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('executei');
+import express, { Request, Response } from 'express';
+
+const server = express();
+const PORT = 3003;
+const HOST = 'http://localhost:';
+
+server.get('/', (req: Request, res:Response) => {
+  res.status(200).send({ foo: 'bar' });
+});
+
+server.listen(3003, () => console.log(`Server is running on ${HOST}${PORT}`)
+);
